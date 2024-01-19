8cd4a9ee6ea9dd89c9bcd71e1b5d04878367382d
Kimdiego2098 <2248356998@qq.com>
2024-01-19 15:07:04 +0800

    更新文档

--- doc/sidebars.js
+++ doc/sidebars.js
@@ -15,28 +15,104 @@ module.exports = {
       id: "2",
       label: "产品介绍"
     },
-    {
-      type: "category",
-      label: "支持作者及Pro版",
-      items: [
-        {
-          type: "doc",
-          id: "1000",
-          label: "支持作者"
-        },
-        {
-          type: "doc",
-          id: "1001",
-          label: "Pro版相关"
-        },
-      ]
-    },
+
     {
       type: "doc",
       id: "3",
       label: "入门指南"
     },
- 
+    {
+      type: "doc",
+      id: "100",
+      label: "驱动调试"
+    },
+    {
+      type: "category",
+      label: "基础手册",
+      items: [
+        {
+          type: "category",
+          label: "网关配置",
+          items: [
+
+            {
+              type: "doc",
+              id: "101",
+              label: "插件管理"
+            },
+            {
+              type: "doc",
+              id: "102",
+              label: "通道管理"
+            },
+            {
+              type: "doc",
+              id: "103",
+              label: "设备"
+            },
+            {
+              type: "doc",
+              id: "104",
+              label: "变量"
+            },
+          ]
+        },
+        {
+          type: "doc",
+          id: "105",
+          label: "网关状态查看"
+        },
+        {
+          type: "doc",
+          id: "106",
+          label: "网关日志"
+        },
+      ]
+    },
+
+    {
+      type: "category",
+      label: "插件手册",
+      items: [
+        {
+          type: "category",
+          label: "Modbus",
+          items: [
+
+            {
+              type: "doc",
+              id: "200",
+              label: "ModbusMaster"
+            },
+            {
+              type: "doc",
+              id: "201",
+              label: "ModbusSlave"
+            },
+          ]
+        },
+
+      ]
+    },
+
+
+    {
+      type: "category",
+      label: "技术支持/合作",
+      items: [
+        {
+          type: "doc",
+          id: "1000",
+          label: "赞助项目"
+        },
+        {
+          type: "doc",
+          id: "1001",
+          label: "Pro版"
+        },
+      ]
+    },
+
   ]
 };
 
