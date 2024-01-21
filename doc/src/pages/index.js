6d82efd41564b08906f42ba7929cc9bef6be7569
Kimdiego2098 <2248356998@qq.com>
2024-01-21 15:23:46 +0800

    更新文档

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -114,7 +114,7 @@ function Gitee() {
             className={"ThingsGateway-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="ThingsGateway-log-number">
-            <div style={{ color: "#723cff" }}>650 +</div>
+            <div style={{ color: "#723cff" }}>600 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
